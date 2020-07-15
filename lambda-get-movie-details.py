e47723ebd32629291b6c943c01ae79791ee5da3b
invokeanand <68322286+invokeanand@users.noreply.github.com>
2020-07-15 19:38:30 +0530

    Update lambda-get-movie-details.py

--- lambda-get-movie-details.py
+++ lambda-get-movie-details.py
@@ -1,13 +1,13 @@
 import boto3
 from boto3.dynamodb.conditions import Key
 def lambda_handler(event, context):
-    TABLE_NAME1 = "netflix"
+    TABLE_NAME1 = "<INSERT TABLE NAME FROM DYNAMODB>"
     Movie_Title=event['title']
     # Creating the DynamoDB Client
-    dynamodb_client = boto3.client('dynamodb', region_name="us-east-1")
+    dynamodb_client = boto3.client('dynamodb', region_name="<REGION>")
 
     # Creating the DynamoDB Table Resource
-    dynamodb = boto3.resource('dynamodb', region_name="us-east-1")
+    dynamodb = boto3.resource('dynamodb', region_name="<REGION>")
     table = dynamodb.Table(TABLE_NAME1)
     response = table.get_item(
         Key={
